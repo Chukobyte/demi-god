3421b1570526a7937ba23261e7c9990b10b308cf
chukobyte <chukobyte@gmail.com>
2023-05-12 14:41:10 -0400

    More conceptualizing level areas.

--- src/level_area_manager.py
+++ src/level_area_manager.py
@@ -1,3 +1,8 @@
+from typing import List, Optional
+
+from src.characters.enemy_definitions import EnemyDefinition
+
+
 class LevelAreaType:
     NORMAL = "normal"
     POWER_UP = "power_up"
@@ -5,8 +10,17 @@ class LevelAreaType:
 
 
 class LevelArea:
-    def __init__(self, width: int):
+    def __init__(self, width: int, enemy_defs: List[EnemyDefinition]):
         self.width = width
+        self.enemy_defs = enemy_defs
+
+
+class LevelAreaDefinitions:
+    DEF_MAP = {1: LevelArea(width=448, enemy_defs=[EnemyDefinition.RABBIT()])}
+
+    @staticmethod
+    def get_def(area_num: int) -> Optional[LevelArea]:
+        return LevelAreaDefinitions.DEF_MAP.get(area_num, None)
 
 
 class LevelAreaManager:
