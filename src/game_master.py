d03a9d35435cad8b8260eed67f31edc45999380b
chukobyte <chukobyte@gmail.com>
2023-05-14 20:34:28 -0400

    More refactoring to move stuff out of game master.

--- src/game_master.py
+++ src/game_master.py
@@ -5,6 +5,7 @@ from crescent_api import *
 from src.characters.player import Player
 from src.characters.wandering_soul import WanderingSoul
 from src.enemy_manager import EnemyManager
+from src.level_area_manager import LevelAreaManager
 from src.level_clouds import LevelCloudManager
 from src.level_state import LevelState
 from src.environment.bridge_gate import BridgeGate
@@ -41,7 +42,6 @@ class GameMaster:
     def __init__(self, main_node):
         self.main = main_node
         self.player: Optional[Player] = None
-        self.spawned_clouds: List[SpawnedCloud] = []
         self.main_task = Task(coroutine=self._update_task())
         self.bridge_transition_task: Optional[Task] = None
 
@@ -52,7 +52,8 @@ class GameMaster:
     async def _update_task(self):
         player_start_pos = Vector2(20, 78)
         enemy_waves_task: Optional[Task] = None
-        level_cloud_manager = LevelCloudManager()
+        level_area_manager = LevelAreaManager()
+        level_cloud_manager = level_area_manager.level_cloud_manager
         try:
             # TODO: put in main.py
             await Task(coroutine=LevelState.fade_transition(time=1.0, fade_out=False))
@@ -71,50 +72,7 @@ class GameMaster:
             self.main.add_child(bridge_gate)
 
             # Shoot down player beam first
-            player_teleport_beam = Sprite.new()
-            player_teleport_beam.texture = Texture(
-                file_path="assets/images/demi/demi_teleport.png"
-            )
-            player_teleport_beam.draw_source = Rect2(0, 0, 48, 48)
-            player_teleport_beam.origin = Vector2(25, 18)
-            player_teleport_beam.z_index = 3
-            self.main.add_child(player_teleport_beam)
-            player_beam_timer = Timer(3.0)
-            player_beam_easer = Easer(
-                Vector2(player_start_pos.x, -20),
-                player_start_pos,
-                player_beam_timer.time,
-                Ease.Cubic.ease_in_vec2,
-            )
-            while True:
-                delta_time = (
-                    Engine.get_global_physics_delta_time() * World.get_time_dilation()
-                )
-                player_beam_timer.tick(delta_time)
-                level_cloud_manager.update()
-                if player_beam_timer.time_remaining > 0.0:
-                    new_beam_pos = player_beam_easer.ease(delta_time)
-                    player_teleport_beam.position = new_beam_pos
-                    await co_suspend()
-                else:
-                    # Hard code animation that delays 2 frames each
-                    player_teleport_beam.draw_source = Rect2(48, 0, 48, 48)
-                    await co_suspend()
-                    await co_suspend()
-                    player_teleport_beam.draw_source = Rect2(0, 0, 48, 48)
-                    await co_suspend()
-                    await co_suspend()
-                    player_teleport_beam.draw_source = Rect2(48, 0, 48, 48)
-                    await co_suspend()
-                    await co_suspend()
-                    player_teleport_beam.draw_source = Rect2(0, 0, 48, 48)
-                    await co_suspend()
-                    await co_suspend()
-                    player_teleport_beam.draw_source = Rect2(48, 0, 48, 48)
-                    await co_suspend()
-                    await co_suspend()
-                    player_teleport_beam.queue_deletion()
-                    break
+            await Task(coroutine=level_area_manager.beam_player_down(player_start_pos))
 
             main_theme_audio_source = AudioManager.get_audio_source(
                 "assets/audio/music/main_theme.wav"
@@ -214,46 +172,5 @@ class GameMaster:
             )
             wandering_soul.z_index = 10
             self.main.add_child(wandering_soul)
-        except GeneratorExit:
-            pass
-
-    async def _manage_clouds_task(self):
-        try:
-            max_clouds = 10
-            cloud_textures = [
-                Texture(file_path="assets/images/environment/cloud_variation1.png"),
-                Texture(file_path="assets/images/environment/cloud_variation2.png"),
-                Texture(file_path="assets/images/environment/cloud_variation3.png"),
-                Texture(file_path="assets/images/environment/cloud_variation4.png"),
-            ]
-            cloud_texture_draw_source = Rect2(0, 0, 32, 18)
-            self.spawned_clouds.clear()
-            # Spawn initial clouds
-            clouds_to_spawn = max_clouds - len(self.spawned_clouds)
-            camera_pos = Camera2D.get_position()
-            for i in range(clouds_to_spawn):
-                cloud = SpawnedCloud.new()
-                cloud.texture = random.choice(cloud_textures)
-                cloud.draw_source = cloud_texture_draw_source
-                cloud.position = camera_pos + Vector2(
-                    i * random.randint(5, 40), random.randint(0, 40)
-                )
-                cloud.z_index = 2
-                self.spawned_clouds.append(cloud)
-                self.main.add_child(cloud)
-            while True:
-                clouds_to_spawn = max_clouds - len(self.spawned_clouds)
-                camera_pos = Camera2D.get_position()
-                for i in range(clouds_to_spawn):
-                    cloud = SpawnedCloud.new()
-                    cloud.texture = random.choice(cloud_textures)
-                    cloud.draw_source = cloud_texture_draw_source
-                    cloud.position = camera_pos + Vector2(
-                        i * random.randint(5, 40), random.randint(0, 40)
-                    )
-                    cloud.z_index = 2
-                    self.spawned_clouds.append(cloud)
-                    self.main.add_child(cloud)
-                await co_wait_seconds(10.0)
         except GeneratorExit:
             pass
--- src/level_area_manager.py
+++ src/level_area_manager.py
@@ -3,8 +3,10 @@ from typing import List, Optional
 from crescent_api import *
 
 from src.characters.enemy_definitions import EnemyDefinition
+from src.characters.player import Player
 from src.characters.wandering_soul import WanderingSoul
 from src.environment.bridge_gate import BridgeGate
+from src.level_clouds import LevelCloudManager
 from src.level_state import LevelState
 from src.utils.game_math import Easer, Ease
 from src.utils.task import co_suspend, co_wait_seconds, Task
@@ -42,8 +44,16 @@ class LevelAreaDefinitions:
     def get_def(area_num: int) -> Optional[LevelArea]:
         return LevelAreaDefinitions.DEF_MAP.get(area_num, None)
 
+    @staticmethod
+    def is_valid_area_index(index: int) -> bool:
+        return index in LevelAreaDefinitions.DEF_MAP.keys()
+
 
 class LevelAreaManager:
+    def __init__(self):
+        self.current_area_index = 0
+        self.level_cloud_manager = LevelCloudManager()
+
     def setup_level_area(self, area: LevelArea) -> None:
         level_state = LevelState()
         level_state.boundary.w += area.width
@@ -55,6 +65,7 @@ class LevelAreaManager:
         level_state.is_gate_transition_queued = True
         level_state.is_currently_transitioning_within_level = True
 
+    # --- TASKS --- #
     async def manage_level_areas(self):
         try:
             level_state = LevelState()
@@ -68,23 +79,84 @@ class LevelAreaManager:
                     if manage_bridge_transition_task:
                         manage_bridge_transition_task.close()
                     manage_bridge_transition_task = Task(
-                        coroutine=self._manage_bridge_transition_task()
+                        coroutine=self._manage_bridge_transition_task(
+                            self.level_cloud_manager
+                        )
                     )
                 if manage_bridge_transition_task:
                     manage_bridge_transition_task.resume()
+
+                self.level_cloud_manager.update()
                 await co_suspend()
         except GeneratorExit:
             pass
 
-    async def _manage_bridge_transition_task(self):
+    async def beam_player_down(self, player_start_pos: Vector2):
         try:
+            # Shoot down player beam first
+            player_teleport_beam = Sprite.new()
+            player_teleport_beam.texture = Texture(
+                file_path="assets/images/demi/demi_teleport.png"
+            )
+            player_teleport_beam.draw_source = Rect2(0, 0, 48, 48)
+            player_teleport_beam.origin = Vector2(25, 18)
+            player_teleport_beam.z_index = 3
+            SceneTree.get_root().add_child(player_teleport_beam)
+            player_beam_timer = Timer(3.0)
+            player_beam_easer = Easer(
+                Vector2(player_start_pos.x, -20),
+                player_start_pos,
+                player_beam_timer.time,
+                Ease.Cubic.ease_in_vec2,
+            )
+            while True:
+                delta_time = (
+                    Engine.get_global_physics_delta_time() * World.get_time_dilation()
+                )
+                player_beam_timer.tick(delta_time)
+                self.level_cloud_manager.update()
+                if player_beam_timer.time_remaining > 0.0:
+                    new_beam_pos = player_beam_easer.ease(delta_time)
+                    player_teleport_beam.position = new_beam_pos
+                    await co_suspend()
+                else:
+                    # Hard code animation that delays 2 frames each
+                    player_teleport_beam.draw_source = Rect2(48, 0, 48, 48)
+                    await co_suspend()
+                    await co_suspend()
+                    player_teleport_beam.draw_source = Rect2(0, 0, 48, 48)
+                    await co_suspend()
+                    await co_suspend()
+                    player_teleport_beam.draw_source = Rect2(48, 0, 48, 48)
+                    await co_suspend()
+                    await co_suspend()
+                    player_teleport_beam.draw_source = Rect2(0, 0, 48, 48)
+                    await co_suspend()
+                    await co_suspend()
+                    player_teleport_beam.draw_source = Rect2(48, 0, 48, 48)
+                    await co_suspend()
+                    await co_suspend()
+                    player_teleport_beam.queue_deletion()
+                    break
+        except GeneratorExit:
+            pass
+
+    async def _manage_bridge_transition_task(
+        self, level_cloud_manager: LevelCloudManager
+    ):
+        try:
+            player: Player = SceneTree.get_root().get_child("Player")
+            self.current_area_index += 1
+            next_level_area = LevelAreaDefinitions.get_def(self.current_area_index)
+            is_last_area = LevelAreaDefinitions.is_valid_area_index(
+                self.current_area_index + 1
+            )
             level_state = LevelState()
-            prev_player_time_dilation = self.player.time_dilation
-            self.player.time_dilation = 0.0
-            for cloud in self.spawned_clouds:
-                cloud.time_dilation = 0.0
-            level_state.boundary.w += 160
-            Camera2D.unfollow_node(self.player)
+            prev_player_time_dilation = player.time_dilation
+            player.time_dilation = 0.0
+            level_cloud_manager.set_clouds_time_dilation(0.0)
+            level_state.boundary.w += next_level_area.width
+            Camera2D.unfollow_node(player)
             Camera2D.set_boundary(level_state.boundary)
             transition_timer = Timer(5.0)
             initial_camera_pos = Camera2D.get_position()
@@ -98,7 +170,7 @@ class LevelAreaManager:
                 Ease.Cubic.ease_out_vec2,
             )
 
-            initial_player_pos = self.player.position
+            initial_player_pos = player.position
             player_dest_pos = initial_player_pos + Vector2(30, 0)
             player_pos_easer = Easer(
                 initial_player_pos,
@@ -108,10 +180,9 @@ class LevelAreaManager:
             )
 
             await co_wait_seconds(1.0)
-            self.player.time_dilation = prev_player_time_dilation
-            self.player.play_animation("walk")
-            for cloud in self.spawned_clouds:
-                cloud.time_dilation = 1.0
+            player.time_dilation = prev_player_time_dilation
+            player.play_animation("walk")
+            level_cloud_manager.set_clouds_time_dilation(1.0)
 
             while True:
                 delta_time = (
@@ -120,27 +191,27 @@ class LevelAreaManager:
                 transition_timer.tick(delta_time)
                 if transition_timer.time_remaining <= 0.0:
                     Camera2D.set_position(dest_camera_pos)
-                    self.player.position = player_dest_pos
+                    player.position = player_dest_pos
                     break
                 else:
                     new_camera_pos = camera_pos_easer.ease(delta_time)
                     Camera2D.set_position(new_camera_pos)
                     new_player_pos = player_pos_easer.ease(delta_time)
-                    self.player.position = new_player_pos
+                    player.position = new_player_pos
                 await co_suspend()
-            level_state.boundary.x = level_state.boundary.w - 160
+            level_state.boundary.x = level_state.boundary.w - next_level_area.width
             Camera2D.set_boundary(level_state.boundary)
-            Camera2D.follow_node(self.player)
-            bridge_gate: BridgeGate = self.main.get_child("Sprite")  # temp
+            Camera2D.follow_node(player)
+            bridge_gate: BridgeGate = SceneTree.get_root().get_child("Sprite")  # temp
             if bridge_gate:
                 bridge_gate.set_closed()
             level_state.is_currently_transitioning_within_level = False
             # Temp wandering soul spawn
-            wandering_soul = WanderingSoul.new()
-            wandering_soul.position = Vector2(
-                level_state.boundary.w - 32, level_state.floor_y
-            )
-            wandering_soul.z_index = 10
-            self.main.add_child(wandering_soul)
+            # wandering_soul = WanderingSoul.new()
+            # wandering_soul.position = Vector2(
+            #     level_state.boundary.w - 32, level_state.floor_y
+            # )
+            # wandering_soul.z_index = 10
+            # self.main.add_child(wandering_soul)
         except GeneratorExit:
             pass
