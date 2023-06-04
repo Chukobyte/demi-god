a8ba5c3ec1bac55f174a8675a38ddbc152061d2e
chukobyte <chukobyte@gmail.com>
2023-06-04 09:37:17 -0400

    Only destroy snake when leaving camera viewport bounds once it passes the player first.

--- src/characters/enemy_snake.py
+++ src/characters/enemy_snake.py
@@ -11,21 +11,29 @@ class EnemySnake(Enemy):
         self.destroy_on_touch = True
         self.physics_update_task = Task(coroutine=self._physics_update_task())
 
+    def _get_move_dir(self, player) -> Vector2:
+        if player.position.x > self.position.x:
+            return Vector2.RIGHT()
+        else:
+            return Vector2.LEFT()
+
     # --- TASKS --- #
     async def _physics_update_task(self) -> None:
         try:
             player = self._find_player()
             move_speed = 30
-            if player.position.x > self.position.x:
-                move_dir = Vector2.RIGHT()
-            else:
+            has_passed_player = False
+            move_dir = self._get_move_dir(player)
+            if move_dir == Vector2.LEFT():
                 self.anim_sprite.flip_h = True
-                move_dir = Vector2.LEFT()
             while True:
-                # TODO: Only check if outside of viewport after it passes the player at least once
-                if self._is_outside_of_camera_viewport():
+                if has_passed_player and self._is_outside_of_camera_viewport():
                     self.destroy()
                     await co_return()
+                else:
+                    dir_to_player = self._get_move_dir(player)
+                    if move_dir != dir_to_player:
+                        has_passed_player = True
                 delta_time = self.get_full_time_dilation_with_physics_delta()
                 self.position += move_dir * Vector2(
                     move_speed * delta_time, move_speed * delta_time
