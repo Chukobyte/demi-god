15f83a33e0ba31c0d3834ad80c20d4ddabda1309
chukobyte <chukobyte@gmail.com>
2023-07-06 22:55:51 -0400

    Set random texture to cloud on reposition.

--- src/level_clouds.py
+++ src/level_clouds.py
@@ -1,9 +1,19 @@
 import random
+from typing import Tuple
 
 from crescent_api import *
+from crescent_api import Vector2
 
 from src.utils.game_math import Ease
 from src.utils.task import co_wait_seconds, Task
+
+
+CLOUD_TEXTURES = [
+    Texture(file_path="assets/images/environment/cloud_variation1.png"),
+    Texture(file_path="assets/images/environment/cloud_variation2.png"),
+    Texture(file_path="assets/images/environment/cloud_variation3.png"),
+    Texture(file_path="assets/images/environment/cloud_variation4.png"),
+]
 
 
 class LevelCloud(Sprite):
@@ -12,6 +22,9 @@ class LevelCloud(Sprite):
         self.move_speed = random.randint(5, 15)
         self.move_dir = Vector2.RIGHT
         self._elapsed_time = 0.0
+
+    def set_random_texture(self) -> None:
+        self.texture = random.choice(CLOUD_TEXTURES)
 
     def _fixed_update(self, delta_time: float) -> None:
         self._elapsed_time += delta_time
@@ -27,9 +40,15 @@ class LevelCloud(Sprite):
             to_pos=new_pos,
             duration=self._elapsed_time + 0.1,
         )
-        self.position = self._attempt_reposition(new_pos)
+        new_pos, has_repositioned = self._attempt_reposition(new_pos)
+        self.position = new_pos
+        # Set random texture when repositioned
+        if has_repositioned:
+            self.set_random_texture()
 
-    def _attempt_reposition(self, position: Vector2, padding=96) -> Vector2:
+    def _attempt_reposition(
+        self, position: Vector2, padding=96
+    ) -> tuple[Vector2, bool]:
         camera_pos = Camera2D.get_position()
         game_props = GameProperties()
         camera_dimension = Size2D(
@@ -37,11 +56,14 @@ class LevelCloud(Sprite):
             game_props.game_resolution.h + camera_pos.y,
         )
         # Check if too far to left
+        has_repositioned = False
         if position.x < camera_pos.x - padding:
             position.x = camera_dimension.w + padding - 32
+            has_repositioned = True
         elif position.x > camera_dimension.w + padding:
             position.x = camera_pos.x - padding + 32
-        return position
+            has_repositioned = True
+        return position, has_repositioned
 
 
 class LevelCloudManager:
@@ -55,12 +77,6 @@ class LevelCloudManager:
     async def _manage_clouds_task(self):
         try:
             max_clouds = 10
-            cloud_textures = [
-                Texture(file_path="assets/images/environment/cloud_variation1.png"),
-                Texture(file_path="assets/images/environment/cloud_variation2.png"),
-                Texture(file_path="assets/images/environment/cloud_variation3.png"),
-                Texture(file_path="assets/images/environment/cloud_variation4.png"),
-            ]
             cloud_texture_draw_source = Rect2(0, 0, 32, 18)
             self.spawned_clouds.clear()
             # Spawn initial clouds
@@ -69,7 +85,7 @@ class LevelCloudManager:
             main_node = SceneTree.get_root()
             for i in range(clouds_to_spawn):
                 cloud = LevelCloud.new()
-                cloud.texture = random.choice(cloud_textures)
+                cloud.set_random_texture()
                 cloud.draw_source = cloud_texture_draw_source
                 cloud.position = camera_pos + Vector2(
                     i * random.randint(5, 40), random.randint(0, 40)
@@ -82,7 +98,7 @@ class LevelCloudManager:
                 camera_pos = Camera2D.get_position()
                 for i in range(clouds_to_spawn):
                     cloud = LevelCloud.new()
-                    cloud.texture = random.choice(cloud_textures)
+                    cloud.set_random_texture()
                     cloud.draw_source = cloud_texture_draw_source
                     cloud.position = camera_pos + Vector2(
                         i * random.randint(5, 40), random.randint(0, 40)
