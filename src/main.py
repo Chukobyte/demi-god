f35886999dfa1501317d69a7ffeab375d7a1a9d5
chukobyte <chukobyte@gmail.com>
2023-07-11 22:24:42 -0400

    Updated background parallax layer.

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 from crescent_api import *
 
 from src.game_master import GameMaster
+from src.utils.task import Task, co_suspend
 
 
 class Main(Node2D):
@@ -8,6 +9,7 @@ class Main(Node2D):
         super().__init__(entity_id=entity_id)
         self.game_master = GameMaster(self)
         self.main_theme_audio_source: Optional[AudioSource] = None
+        self.ground_scroll_task: Optional[Task] = None
 
     def _start(self) -> None:
         max_game_width = 9999.0
@@ -25,6 +27,9 @@ class Main(Node2D):
         buildings_draw_source = buildings.draw_source
         buildings_draw_source.w = max_game_width
         buildings.draw_source = buildings_draw_source
+        bg_ground: Sprite = self.get_child("Parallax").get_child("Ground")
+        bg_ground.draw_source = buildings_draw_source
+        self.ground_scroll_task = Task(coroutine=self._ground_scroll_task(bg_ground))
 
     def _update(self, delta_time: float) -> None:
         if Input.is_action_just_pressed("exit"):
@@ -32,3 +37,21 @@ class Main(Node2D):
 
     def _fixed_update(self, delta_time: float) -> None:
         self.game_master.update()
+        if self.ground_scroll_task:
+            self.ground_scroll_task.resume()
+
+    async def _ground_scroll_task(self, bg_ground: Sprite):
+        try:
+            move_speed = 2
+            while True:
+                delta_time = World.get_time_dilation() * World.get_delta_time()
+                new_pos = bg_ground.position
+                new_pos += Vector2.RIGHT * Vector2(delta_time * move_speed, 0)
+                # Prevent from running out of image
+                if new_pos.x >= 0.0:
+                    new_pos.x = -6400.0
+                bg_ground.position = new_pos
+
+                await co_suspend()
+        except GeneratorExit:
+            pass
