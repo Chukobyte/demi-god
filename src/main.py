a37386006a01ebf522a3ec3aee43712d1616ee85
chukobyte <chukobyte@gmail.com>
2023-07-15 21:11:05 -0400

    Cleaning up redundant code in main.py _start function.

--- src/main.py
+++ src/main.py
@@ -12,30 +12,8 @@ class Main(Node2D):
         self.ground_scroll_task: Optional[Task] = None
 
     def _start(self) -> None:
-        max_game_width = 9999.0
-        bg_color_rect: ColorRect = self.get_child("BGColorRect")
-        bg_color_rect.size = Size2D(max_game_width, bg_color_rect.size.h)
-        # Bridge Ground
-        bridge_ground: Sprite = self.get_child("BridgeGround")
-        bridge_ground_draw_source = bridge_ground.draw_source
-        bridge_ground_draw_source.w = max_game_width
-        bridge_ground.draw_source = bridge_ground_draw_source
-        bridge_ground_bottom_railings: Sprite = self.get_child("BottomRailings")
-        bridge_ground_bottom_railings.draw_source = bridge_ground_draw_source
-        # Buildings Background
-        # buildings: AnimatedSprite = self.get_child("Parallax").get_child("Buildings")
-        # buildings_draw_source = buildings.draw_source
-        # buildings_draw_source.w = max_game_width
-        # buildings.draw_source = buildings_draw_source
-        buildings_draw_source = Rect2(0.0, 0.0, 320.0, 144.0)
-        buildings_draw_source.w = max_game_width
-        # Ground
         bg_ground: Sprite = self.get_child("ParallaxBack").get_child("Ground")
-        bg_ground.draw_source = buildings_draw_source
         self.ground_scroll_task = Task(coroutine=self._ground_scroll_task(bg_ground))
-        # Back buildings
-        back_buildings: Sprite = self.get_child("ParallaxBack").get_child("Buildings")
-        back_buildings.draw_source = buildings_draw_source
 
     def _update(self, delta_time: float) -> None:
         if Input.is_action_just_pressed("exit"):
