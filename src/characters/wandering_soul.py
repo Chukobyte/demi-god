cb693921f5fb398bddb37879e0bfe6dd103462cc
chukobyte <chukobyte@gmail.com>
2023-07-28 15:22:21 -0400

    Adding rough wandering soul capsule.

--- src/characters/wandering_soul.py
+++ src/characters/wandering_soul.py
@@ -12,3 +12,19 @@ class WanderingSoul(Node2D):
         self.anim_sprite = self.get_child("AnimatedSprite")
         if self.anim_sprite:
             self.anim_sprite.flip_h = self.flip_h
+
+
+class WanderingSoulCapsule(Node2D):
+    def __init__(self, entity_id: int):
+        super().__init__(entity_id)
+        self.sprite: Optional[Sprite] = None
+
+    def _start(self) -> None:
+        size = Size2D(48, 48)
+        # Sprite
+        self.sprite = Sprite.new()
+        self.sprite.texture = Texture(
+            "assets/images/environment/wandering_soul_capsule.png"
+        )
+        self.sprite.draw_source = Rect2(0, 0, size.w, size.h)
+        self.add_child(self.sprite)
